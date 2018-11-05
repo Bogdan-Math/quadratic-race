81d07c331b242011d89ecfe170032c5ee6cebb29
Bohdan Stepanov <bohdan.math.stepanov@gmail.com>
2018-11-05 18:35:06 +0200

    add javafx tests

--- src/main/java/race/HelloFX.java
+++ src/main/java/race/HelloFX.java
@@ -5,21 +5,18 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-
 public class HelloFX extends Application {
-
-    @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(l, 320, 240);
-        stage.setScene(scene);
-        stage.show();
-    }
 
     public static void main(String[] args) {
         launch(args);
     }
 
+    @Override
+    public void start(Stage stage) {
+        Label l = new Label("Hello, JavaFX, running on Java 11");
+        l.setId("lab1");
+        Scene scene = new Scene(l, 320, 240);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
---
+++ src/test/java/race/HelloFXTest.java
@@ -0,0 +1,32 @@
+package race;
+
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import static org.testfx.assertions.api.Assertions.assertThat;
+
+public class HelloFXTest extends ApplicationTest {
+
+//    @BeforeClass
+//    public static void beforeClass() {
+//        System.setProperty("testfx.headless", "true");
+//    }
+
+    @Override
+    public void start(Stage stage) {
+        new HelloFX()
+                .start(stage);
+
+        stage.show();
+    }
+
+    @Test
+    public void name() {
+//        clickOn("#b1");
+        assertThat(lookup("#lab1").queryAs(Label.class))
+                .hasText("Hello, JavaFX, running on Java 11");
+    }
+}
